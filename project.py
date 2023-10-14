af5ff576dda13b9ff5bb229f2a2d6409a7da00c7
Shane Fernandes <shanesf5@gmail.com>
2023-10-15 00:59:56 +0530

    Setup Flask, Flask template, Flask static files

--- project.py
+++ project.py
@@ -1,18 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
 def main():
-    ...
+    app.run(debug=True)
 
-
-def function_1():
-    ...
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 def function_2():
     ...
 
-
 def function_n():
     ...
-
 
 if __name__ == "__main__":
     main()
