3e6ba35a566deed94aace91f2fb92653879c2a06
Shane Fernandes <shanesf5@gmail.com>
2023-10-15 08:27:52 +0530

    Add css for card, Limit words in card

--- project.py
+++ project.py
@@ -1,5 +1,5 @@
-from flask import Flask, jsonify, render_template, request
-import requests
+from flask import Flask, render_template, request
+import requests, random
 
 app = Flask(__name__)
 
@@ -14,15 +14,24 @@ def index():
 def genre():
     genre_id = request.values.get('selected')
 
-    response = requests.get(f"https://api.jikan.moe/v4/anime?type=tv&rating=g&rating=pg&rating=pg13&order_by=popularity&genres={genre_id}&limit=1")
-    print(response)
+    response = requests.get(f"https://api.jikan.moe/v4/anime?type=tv&rating=g&rating=pg&rating=pg13&order_by=popularity&genres={genre_id}&limit=20")
+
     if response.status_code == 200:
         anime_list = response.json()
-        list_data=anime_list["data"][0]
-        image = list_data["images"]["jpg"]["image_url"]
-        title = list_data["title"]
-        desc = list_data["synopsis"].split('\n')[0]
-
+        list_data=anime_list["data"]
+        list_data=random.choices(list_data)
+        image = list_data[0]["images"]["jpg"]["image_url"]
+        title = list_data[0]["title"]
+        desc = list_data[0]["synopsis"].split('\n')[0]
+        duplicate = desc
+        
+        desc=''
+        for index,word in enumerate(duplicate.split(' ')):
+            desc+=word+" "
+            if index==79:
+                desc+= "..."
+                break
+        
         data=[{'image':image,'title':title,'desc':desc}]
 
         return data
